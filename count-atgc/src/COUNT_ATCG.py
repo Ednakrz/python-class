102da11e643f9e07d8bbdc9409d1e4b427fd5c67
Ednakrz <160920197+Ednakrz@users.noreply.github.com>
2024-04-11 18:00:06 -0600

    Se cambió el código  para que acepte un archivo proporcionado por el usuario

--- count-atgc/src/COUNT_ATCG.py
+++ count-atgc/src/COUNT_ATCG.py
@@ -29,6 +29,15 @@ Ejemplo
   Linea de comando usada para el ejemplo: 
   python COUNT_ATCG.py dna_sequence.txt
 '''
+import sys
+
+# Verificar si se proporcionó el nombre del archivo como argumento
+if len(sys.argv) != 2:
+    print("Uso: python count_atgc.py <nombre_del_archivo>")
+    sys.exit(1)
+
+# Obtener el nombre del archivo de los argumentos de la línea de comandos
+nombre_archivo = sys.argv[1]
 
 # Inicializar contadores para cada símbolo
 count_A = 0
@@ -36,27 +45,34 @@ count_T = 0
 count_G = 0
 count_C = 0
 
-# Abrir el archivo en modo lectura
-with open('archivo.txt', 'r') as file:
-    # Leer la cadena del archivo y convertirla a mayúsculas
-    cadena = file.read().upper()
+try:
+    # Abrir el archivo en modo lectura
+    with open(nombre_archivo, 'r') as file:
+        # Leer la cadena del archivo y convertirla a mayúsculas
+        cadena = file.read().upper()
 
-# Contar las ocurrencias de cada símbolo en la cadena
-for letra in cadena:
-    if letra == 'A':
-        count_A += 1
-    elif letra == 'T':
-        count_T += 1
-    elif letra == 'G':
-        count_G += 1
-    elif letra == 'C':
-        count_C += 1
+    # Contar las ocurrencias de cada símbolo en la cadena
+    for letra in cadena:
+        if letra == 'A':
+            count_A += 1
+        elif letra == 'T':
+            count_T += 1
+        elif letra == 'G':
+            count_G += 1
+        elif letra == 'C':
+            count_C += 1
 
-# Imprimir el resultado
-print(f'El símbolo A aparece p veces en la cadena.')
-print(f'El símbolo T aparece {count_T} veces en la cadena.')
-print(f'El símbolo G aparece {count_G} veces en la cadena.')
-print(f'El símbolo C aparece {count_C} veces en la cadena.')
+    # Imprimir el resultado
+    print(f'El símbolo A aparece {count_A} veces en la cadena.')
+    print(f'El símbolo T aparece {count_T} veces en la cadena.')
+    print(f'El símbolo G aparece {count_G} veces en la cadena.')
+    print(f'El símbolo C aparece {count_C} veces en la cadena.')
+
+except FileNotFoundError:
+    print(f"Error: No se pudo encontrar el archivo '{nombre_archivo}'.")
+
+except:
+    print("Error inesperado:", sys.exc_info()[0])
 
 
 
