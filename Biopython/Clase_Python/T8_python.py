103eeebe16149c4cbde565b2a1a5f0c45e310cc1
Ednakrz <ednakrz@lcg.unam.mx>
2024-06-02 10:39:49 -0600

    Edición del programa para que ahora me de un archivo de salida con los codones de los diferentes marcos de lectura

--- Biopython/Clase_Python/T8_python.py
+++ Biopython/Clase_Python/T8_python.py
@@ -1,15 +1,26 @@
+
+
+
+
+
+
+
 import re
 from Bio import SeqIO
 from Bio.Seq import Seq
  
+
 for record in SeqIO.parse("seq.nt.fa", "fasta"):
-    seq = record.seq
-    print ("ID:",record.id)
-    codon = re.findall(r"(.{3})", str(seq))
-    codon_str = ' '.join(codon)
-    print("Codones:",codon_str) 
-    print("Secuencia:", record.seq)
-    print("Longitud de la secuencia:\n", len(record.seq))
+    seq_id = record.id
+    output_filename = f"frame_{seq_id}.txt"
+    with open(output_filename, "w") as output_file: 
+        output_file.write("ID: " + record.id + "\n")
+        seq = record.seq
+        codon = re.findall(r"(.{3})", str(seq))
+        codon_str = ' '.join(codon)
+        output_file.write("Codones: " + codon_str + "\n")
+        output_file.write("Secuencia: " + str(record.seq) + "\n")
+        output_file.write("Longitud de la secuencia: " + str(len(record.seq)) + "\n")
 
 
     
