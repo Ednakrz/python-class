8fd4294aea04ea29ef8cf8bffef997b18bf885b8
Ednakrz <ednakrz@lcg.unam.mx>
2024-06-02 10:49:19 -0600

    Agregué la documentación al programa

--- Biopython/Clase_Python/T8_python.py
+++ Biopython/Clase_Python/T8_python.py
@@ -1,10 +1,38 @@
+'''
+NOMBRE
+T8_python
+VERSION
+  1.0 
+
+AUTOR
+ Edna Karen Rivera Zagal <ednakrz@lcg.unam.mx>
+
+ SINOPSIS
+    python .\T8_python.py
+
+DESCRIPCION
+   Este programa está diseñado pora obtener los codones de las secuencias del archivo Fasta 'seq.nt.fa', y devolver cada uno de estos resultados 
+   en un archivo de salida correspondiente a cada marco de lectura dentro de mi archivo.
 
 
+CATEGORIA
+    Bioiformática
 
+OPCIONES 
+    Este programa no cuenta con opciones para ingresar datos por el memento, por favor sigue el ejemplo de la forma para ejecutar el programa. 
+USO 
+Forma en la que se puede ejecutar el programa: 
+  ejemplo: python .\T8_python.py
 
+Ejemplo
+  Linea de comando usada para el ejemplo: 
+  python .\T8_python.py
 
+  El progrma no devuelve algo dentro de la terminal, pero en el repostorio donde está el programa y el archivo origen se encontrarán los archivos de 
+  salida. 
 
-
+  frame_{seq_id}.txt
+'''
 import re
 from Bio import SeqIO
 from Bio.Seq import Seq
